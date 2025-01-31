0210a4591022c328c7fce7907cfdb1841f3a9522
HariDharsaun <haridharsaun18personal@gmail.com>
2025-01-31 15:01:57 +0530

    ready for deploy

--- symtoms-Checker/server/models/Users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const userSchema = new mongoose.Schema({
-    symptom:String,
-    disease:String,
-    advice:String
-})
-
-const UserModel = mongoose.model('symptoms',userSchema)
-module.exports = UserModel
